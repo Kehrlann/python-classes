e00b19bbfeeff6758e6ed890e87b2fed97cf2ecc
Daniel Garnier-Moiroux <git@garnier.wf>
2020-11-05 14:26:00 +0100

    Student: add Class class

--- student.py
+++ student.py
@@ -7,3 +7,17 @@ class Student:
 
     def __repr__(self):
         return "PAS ENCORE IMPLEMENTE"
+
+
+class Class:
+    def __init__(self, classname: str):
+        pass
+
+    def add_student(self, student: Student):
+        pass
+
+    def __len__(self):
+        return 0
+
+    def __repr__(self):
+        return f""
