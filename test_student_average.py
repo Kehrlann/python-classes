e1b83fc10d26c6dcfe61bce967873aa64ece2807
Daniel Garnier-Moiroux <git@garnier.wf>
2020-11-05 14:13:13 +0100

    Simplify student test average

--- test_student_average.py
+++ test_student_average.py
@@ -1,47 +1,31 @@
 import unittest
 from student import Student
-from testing_utils import BaseTestCase
-
-achille = Student("Achille", "Talon")
 
 
-def average(student, topic):
-    return student.compute_average(topic)
+class TestStudentTopics(unittest.TestCase):
 
+    def test_one_grade(self):
+        student = Student("a", "b")
+        student.add_grade("Math", 12.)
+        self.assertEqual(student.compute_average("Math"), 12.)
 
-class TestStudentTopics(BaseTestCase):
+    def test_many_grades(self):
+        student = Student("a", "b")
+        grades = [8., 10., 12., 12.]
+        for grade in grades:
+            student.add_grade("History", grade)
+        self.assertEqual(
+            student.compute_average("History"),
+            10.5,
+            f"for grades {grades}, average should be 10.5")
 
-    def test_student_topics(self):
-        self.__preflight_checks()
-
-        one = Student("one", "one")
-        one.add_grade("History", 10.)
-        one.add_grade("History", 12.)
-        two = Student("two", "two")
-        two.add_grade("Math", 12.)
-
-        test_cases = [
-            (one, "History", 11.),
-            (one, "French", -1.),
-            (two, "Math", 12.)
-        ]
-        self.run_tests(test_cases, average)
-
-    def ok_message(self, args, expected):
-        print(
-            f"OK    : For grades {args[1:]} -> topics \"{expected}\"")
-
-    def error_message(self, args, expected, result):
-        _, *grades = args
-        print(
-            f"ERROR: For grades {args[1:]} -> topics should be \"{expected}\", but were \"{result}\"")
-
-    def __preflight_checks(self):
-        # Add grade exists
-        if "compute_average" not in Student.__dict__:
-            print("ERROR: there is no 'compute_average' method in class 'Student'")
-            self.fail(
-                "ERROR: there is no 'compute_average' method in class 'Student'")
+    def test_missing_topic(self):
+        student = Student("a", "b")
+        self.assertEqual(
+            student.compute_average("Unknown topic"),
+            -1.,
+            f"when student hasn't studied a topic, average should be -1"
+        )
 
 
 if __name__ == '__main__':
